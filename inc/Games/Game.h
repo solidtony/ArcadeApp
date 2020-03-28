2ad6b2cf870da18963a5115d04c6922e51a9540e
Anthony Garcia <anthonygarcia918@hotmail.com>
2020-03-28 06:06:34 -0600

    Added Game Interface and GameScene implementation of Scene Interface

---
+++ inc/Games/Game.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <stdint.h>
+#include <string>
+
+class GameController;
+class Screen;
+
+// Game Interface
+class Game
+{
+public:
+	virtual ~Game(){}
+	virtual void Init(GameController& controller) = 0;
+	virtual void Update(uint32_t dt) = 0;
+	virtual void Draw(Screen& screen) = 0;
+	virtual std::string GetName() const = 0;
+};
---
+++ inc/Scenes/GameScene.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <memory>
+
+#include "Games/Game.h"
+#include "Scene.h"
+ 
+class GameScene : public Scene
+{
+public:
+	GameScene(std::unique_ptr<Game> optrGame);
+	virtual ~GameScene() {}
+
+	virtual void Init() override;
+	virtual void Update(uint32_t dt) override;
+	virtual void Draw(Screen& screen) override;
+
+	virtual const std::string& GetSceneName() const override;
+
+private:
+	std::unique_ptr<Game> mGame;
+
+};
