f806f691df88fc3c8e19fda990fc5e89ccdc6cec
Anthony Garcia <anthonygarcia918@hotmail.com>
2020-11-24 16:13:33 -0700

    Added button class.

---
+++ inc/App/Button.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <string>
+#include "Shapes/AARectangle.h"
+#include <functional>
+#include "Graphics/Color.h"
+#include "Graphics/BitmapFont.h"
+
+class Screen;
+
+class Button
+{
+public:
+	using ButtonAction = std::function<void(void)>;
+
+	Button(const BitmapFont& bitmapFont, const Color& textColor, const Color& highlightColor = Color::White());
+	void Init(Vec2D topLeft, unsigned int width, unsigned height);
+	void Draw(Screen& theScreen);
+	void ExecuteAction();
+
+	inline void SetButtonText(const std::string& text) { mTitle = text; }
+	inline const std::string& GetButtonText() const { return mTitle; }
+	inline void SetIsHighlighted(bool isHighlighted) { mIsHighlighted = isHighlighted; }
+	inline bool IsHighlighted() const { return mIsHighlighted; }
+	inline void SetButtonAction(ButtonAction buttonAction) { mAction = buttonAction; }
+
+private:
+	const BitmapFont& mBitmapFont;
+	std::string mTitle;
+	AARectangle mBBox;
+
+	bool mIsHighlighted;
+	ButtonAction mAction;
+	Color mHighlightColor;
+	Color mTextColor;
+};
---
+++ src/App/Button.cpp
@@ -0,0 +1,29 @@
+#include "App/Button.h"
+
+#include "Graphics/Screen.h"
+
+Button::Button(const BitmapFont& bitmapFont, const Color& textColor, const Color& highlightColor) :
+mBitmapFont(bitmapFont),  mTextColor(textColor), mHighlightColor(highlightColor), mTitle("")
+{
+
+}
+
+void Button::Init(Vec2D topLeft, unsigned int width, unsigned height)
+{
+	mBBox = AARectangle(topLeft, width, height);
+}
+
+void Button::Draw(Screen& theScreen)
+{
+	theScreen.Draw(mBitmapFont, mTitle, mBitmapFont.GetDrawPosition(mTitle, mBBox, BFXA_CENTER, BFYA_CENTER), mTextColor);
+
+	if (mIsHighlighted)
+	{
+		theScreen.Draw(mBBox, mHighlightColor);
+	}
+}
+
+void Button::ExecuteAction()
+{
+	mAction();
+}
