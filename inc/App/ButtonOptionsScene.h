a1d8dce4c29b9796b92a4b21d66d77badfe22b22
Anthony Garcia <anthonygarcia918@hotmail.com>
2020-11-24 17:12:45 -0700

    Added button options scene

---
+++ inc/App/ButtonOptionsScene.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "App/Button.h"
+#include "Scenes/Scene.h"
+
+class Screen;
+
+class ButtonOptionsScene : public Scene
+{
+public:
+	ButtonOptionsScene(const std::vector<std::string>& optionNames, const Color& textColor);
+
+	virtual void Init() override;
+	virtual void Update(uint32_t dt) override;
+	virtual void Draw(Screen& theScreen) override;
+
+	void SetButtonActions(const std::vector<Button::ButtonAction>& buttonActions);
+
+private:
+	void SetNextButtonHighlighted();
+	void SetPreviousButtonHighlighted();
+	void ExecuteCurrentButtonAction();
+	void HighlightCurrentButton();
+
+	std::vector<Button> mButtons;
+	int mHighlightedOption;
+
+};
