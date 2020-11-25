42ae3961ddc48bd6a0c08098886a8ee45c58bf79
Anthony Garcia <anthonygarcia918@hotmail.com>
2020-11-24 17:30:43 -0700

    Added a scene for non implemented scenes.

---
+++ inc/Scenes/NotImplementedScene.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Scenes/Scene.h"
+
+class Screen;
+
+class NotImplementedScene : public Scene
+{
+public:
+	virtual void Init() override;
+	virtual void Update(uint32_t dt) override;
+	virtual void Draw(Screen& theScreen) override;
+	virtual const std::string& GetSceneName() const override;
+};
