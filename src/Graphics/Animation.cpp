9436c5c6c8906afa83bef51a63cabeeb640e0cc7
Anthony Garcia <anthonygarcia918@hotmail.com>
2020-12-03 17:37:08 -0700

    The Anmiation Class

---
+++ src/Graphics/Animation.cpp
@@ -0,0 +1,43 @@
+#include "Graphics/Animation.h"
+
+Animation::Animation() : mSize(Vec2D::Zero()), mSpriteSheetName(""), mAnimationName(""), mFPS(0)
+{
+
+}
+
+AnimationFrame Animation::GetAnimationFrame(uint32_t frameNum) const
+{
+	AnimationFrame frame;
+	
+	if (frameNum > mFrames.size())
+	{
+		return frame;
+	}
+
+	frame.frame = mFrames[frameNum];
+	
+	if (frameNum < mFrameColors.size())
+	{
+		frame.frameColor = mFrameColors[frameNum];
+		frame.frameColorSet = 1;
+	}
+
+	if (mOverlay.size() > 0)
+	{
+		frame.overlay = mOverlay;
+	}
+
+	if (frameNum < mOverlayColors.size())
+	{
+		frame.overlayColor = mOverlayColors[frameNum];
+	}
+
+	if (frameNum < mFrameOffsets.size())
+	{
+		frame.offset = mFrameOffsets[frameNum];
+	}
+
+	frame.size = mSize;
+
+	return frame;
+}
