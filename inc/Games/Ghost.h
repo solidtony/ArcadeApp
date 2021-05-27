48296e4eec458002f123aa031790975fc31d97cc
Anthony Garcia <anthonygarcia918@hotmail.com>
2021-05-27 16:26:07 -0600

    Ghost Class

---
+++ inc/Games/Ghost.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include "Games/Actor.H"
+#include "Graphics/Color.h"
+#include "Games/PacmanGameUtils.h"
+#include "Shapes/AARectangle.h"
+
+enum GhostName
+{
+	BLINKY = 0,
+	PINKY,
+	INKY,
+	CLYDE,
+	NUM_GHOSTS
+};
+
+enum GhostState
+{
+	ALIVE = 0,
+	VULNERABLE,
+	VULNERABLE_ENDING,
+	DEAD
+};
+
+class Ghost : public Actor
+{
+public:
+	static const uint32_t VULNERABILITY_TIME = 6000;
+	static const uint32_t VULNERABILITY_ENDING_TIME = 4000;
+
+	Ghost();
+	virtual void Init(const SpriteSheet& spriteSheet, const std::string& animationsPath, const Vec2D& initialPos, uint32_t movementSpeed, bool updateSpriteOnMovement, const Color& spriteColor = Color::White()) override;
+	virtual void Update(uint32_t dt) override;
+
+	void SetStateToVulnerable();
+	virtual void SetMovementDirection(PacmanMovement direction) override;
+	virtual void Stop() override;
+
+	void EatenByPacman();
+	void ResetToFirstPostion();
+
+	inline bool IsDead() const { return mState == GhostState::DEAD; }
+	inline bool IsVulnerable() const { return mState == GhostState::VULNERABLE || mState == GhostState::VULNERABLE_ENDING; }
+	inline bool IsAlive() const { return mState == GhostState::ALIVE; }
+	inline uint32_t GetPoints() const { return mPoints; }
+	inline void LockCanChangeDirection() { mCanChangeDirection = false; }
+
+private:
+	void SetGhostState(GhostState state);
+
+	uint32_t mVulnerabilityTimer;
+	uint32_t mPoints;
+	GhostState mState;
+	bool mCanChangeDirection;
+	Vec2D mInitialPos;
+};
---
+++ src/Games/Ghost.cpp
@@ -0,0 +1,141 @@
+#include "Games/Ghost.h"
+
+namespace
+{
+	const uint32_t NUM_POINTS_FOR_GHOST = 200;
+}
+
+Ghost::Ghost(): mPoints(0), mInitialPos(Vec2D::Zero())
+{
+
+}
+
+void Ghost::Init(const SpriteSheet& spriteSheet, const std::string& animationsPath, const Vec2D& initialPos, uint32_t movementSpeed, bool updateSpriteOnMovement, const Color& spriteColor)
+{
+	Actor::Init(spriteSheet, animationsPath, initialPos, movementSpeed, updateSpriteOnMovement, spriteColor);
+
+	mInitialPos = initialPos;
+	mPoints = NUM_POINTS_FOR_GHOST;
+	ResetToFirstPostion();
+}
+
+void Ghost::Update(uint32_t dt)
+{
+	Vec2D position = Position();
+
+	Actor::Update(dt);
+	mCanChangeDirection = position != Position();
+
+	if (IsVulnerable())
+	{
+		mVulnerabilityTimer += dt;
+
+		if (mState == GhostState::VULNERABLE && mVulnerabilityTimer >= VULNERABILITY_TIME)
+		{
+			SetGhostState(GhostState::VULNERABLE_ENDING); // resets the vulnerability timer back to zero
+		}
+
+		if (mState == GhostState::VULNERABLE_ENDING && mVulnerabilityTimer >= VULNERABILITY_ENDING_TIME)
+		{
+			SetGhostState(GhostState::ALIVE);
+		}
+	}
+}
+
+void Ghost::SetStateToVulnerable()
+{
+	if (mState != GhostState::DEAD)
+	{
+		SetGhostState(GhostState::VULNERABLE);
+	}
+}
+
+void Ghost::SetMovementDirection(PacmanMovement direction)
+{
+	Actor::SetMovementDirection(direction);
+
+	PacmanMovement movementDir = GetMovementDirection();
+
+	if (mState == GhostState::ALIVE)
+	{
+		if (movementDir == PacmanMovement::RIGHT)
+		{
+			SetAnimation("ghost_movement_right", true);
+		}
+		else if (movementDir == PacmanMovement::UP)
+		{
+			SetAnimation("ghost_movement_up", true);
+		}
+		else if (movementDir == PacmanMovement::DOWN)
+		{
+			SetAnimation("ghost_movement_down", true);
+		}
+		else if (movementDir == PacmanMovement::LEFT)
+		{
+			SetAnimation("ghost_movement_left", true);
+		}
+	}
+	else if (mState == GhostState::DEAD)
+	{
+		if (movementDir == PacmanMovement::RIGHT)
+		{
+			SetAnimation("ghost_dead_right", true);
+		}
+		else if (movementDir == PacmanMovement::UP)
+		{
+			SetAnimation("ghost_dead_up", true);
+		}
+		else if (movementDir == PacmanMovement::DOWN)
+		{
+			SetAnimation("ghost_dead_down", true);
+		}
+		else if (movementDir == PacmanMovement::LEFT)
+		{
+			SetAnimation("ghost_dead_left", true);
+		}
+	}
+}
+
+void Ghost::Stop()
+{
+	SetMovementDirection(PacmanMovement::NONE);
+}
+
+void Ghost::EatenByPacman()
+{
+	SetGhostState(GhostState::DEAD);
+}
+
+void Ghost::ResetToFirstPostion()
+{
+	mSprite.SetPosition(mInitialPos);
+	SetMovementDirection(PacmanMovement::NONE);
+	mVulnerabilityTimer = 0;
+	SetGhostState(GhostState::ALIVE);
+	mCanChangeDirection = true;
+}
+
+void Ghost::SetGhostState(GhostState state)
+{
+	mState = state;
+	switch (mState)
+	{
+	case ALIVE:
+		SetMovementDirection(GetMovementDirection());
+		SetMovementSpeed(GHOST_MOVEMNET_SPEED);
+		break;
+	case VULNERABLE:
+		mSprite.SetAnimation("ghost_vulnerable", true);
+		mVulnerabilityTimer = 0;
+		SetMovementSpeed(GHOST_VULNERABLE_MOVEMENT_SPEED);
+		break;
+	case VULNERABLE_ENDING:
+		mSprite.SetAnimation("ghost_vulnerable_ending", true);
+		mVulnerabilityTimer = 0;
+		break;
+	case DEAD:
+		SetMovementDirection(GetMovementDirection());
+		SetMovementSpeed(GHOST_BACK_TO_PEN_SPEED);
+		break;
+	}
+}
